3597c570e26930bb9a998d9c45c5096a08fecf25
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-12-01 17:10:23 +0100

    Document common library functions

--- aoc-common/src/input.rs
+++ aoc-common/src/input.rs
@@ -1,14 +1,17 @@
 use std::io::{self, BufRead};
 
+/// Handles input reading and collecting.
 pub struct Input {
     lines: Vec<String>,
 }
 
 impl Input {
+    /// Basic constructor to construct a new instance.
     pub fn new() -> Self {
         return Input { lines: Vec::new() };
     }
 
+    /// Reads the entire standard input stream line-by-line.
     pub fn read_lines(&mut self) -> &Self {
         let mut lines: Vec<String> = Vec::new();
 
@@ -21,6 +24,7 @@ impl Input {
         return self;
     }
 
+    /// Returns the read lines with each line converted into a number where possible.
     pub fn lines_as_numbers(&self) -> Vec<Option<isize>> {
         return self.lines.iter().map(|s| s.parse::<isize>().ok()).collect();
     }
--- aoc-common/src/vectors.rs
+++ aoc-common/src/vectors.rs
@@ -1,3 +1,6 @@
+/// Groups a vector of optional T instances. Consecutive Some values are grouped into
+/// a single sub-array. When encountering a None value, finishes the current sub-array
+/// and starts a new one.
 pub fn group_option_vector<T>(vector: Vec<Option<T>>) -> Vec<Vec<T>> {
     let mut result: Vec<Vec<T>> = Vec::new();
 
