e2f2f9a0000829eddf94df378687227a633597d5
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-12-16 22:53:47 +0100

    Try solving day 16 part 2. Does not work yet.

--- day-16-dieciseis/src/graph.rs
+++ day-16-dieciseis/src/graph.rs
@@ -39,7 +39,7 @@ impl Graph {
         return Graph { nodes };
     }
 
-    pub fn solve_task(&self, time: usize) -> usize {
+    pub fn solve_task_v1(&self, time: usize) -> usize {
         // 2D table, each value contains our largest pressure release where
         // the Y coordinate (outer index) is the elapsed time in minutes - 1,
         // the X coordinate (inner index, or hash key) is the name of the node.
@@ -47,7 +47,7 @@ impl Graph {
         // a HashSet, containing the list of opened valves and an usize (which is the pressure release value).
         let mut table: Vec<HashMap<String, (bool, HashSet<String>, usize)>> = vec![HashMap::new(); time + 1];
 
-        println!("Simulating minute 1...");
+        println!("Simulating minute 0...");
 
         // In zero minutes, every value is 0, and only AA can be reached.
         for (name, _) in &self.nodes {
@@ -96,9 +96,102 @@ impl Graph {
                 print!("| {} {} {} ", k, v.0, v.2);
             }
             println!();
+        }*/
+
+        return table[time].iter().max_by(|a, b| a.1.2.cmp(&b.1.2)).unwrap().1.2;
+    }
+
+    pub fn solve_task_v2(&self, time: usize) -> usize {
+        // 2D table, each value contains our largest pressure release where
+        // the Y coordinate (outer index) is the elapsed time in minutes - 1,
+        // the X coordinate (inner index, or hash key) is the name of the node pair (the node of you and the elephant).
+        // Each entry contains a bool (whether this cell combination can be reached, starting from ("AA", "AA"))
+        // a HashSet, containing the list of opened valves and an usize (which is the pressure release value).
+        let mut table: Vec<HashMap<(String, String), (bool, HashSet<String>, usize)>> = vec![HashMap::new(); time + 1];
+
+        println!("Simulating minute 0...");
+
+        // In zero minutes, every value is 0, and only AA can be reached.
+        for (name1, _) in &self.nodes {
+            for (name2, _) in &self.nodes {
+                table[0].insert((name1.to_string(), name2.to_string()),
+                    (name1 == "AA" && name2 == "AA", HashSet::new(), 0));
+            }
         }
 
-        println!("{:?}", table[time - 1].iter().max_by(|a, b| a.1.2.cmp(&b.1.2)).unwrap());*/
-        return table[time - 1].iter().max_by(|a, b| a.1.2.cmp(&b.1.2)).unwrap().1.2;
+        // In the remaining minutes.
+        for min in 1..=time {
+            println!("Simulating minute {}...", min);
+
+            for (name1, node1) in &self.nodes {
+                for (name2, node2) in &self.nodes {
+                    // One way to raise our release value is to open the valve in the node we're in.
+                    // If it was already opened, we must have moved in the previous step, so this
+                    // is not applicable.
+
+                    // But, opening the valve is possible in two ways: you open it while the elephant
+                    // moves, OR you move while the elephant opens it. And moving has many possibilities.
+                    let mut route_one = (false, HashSet::new(), 0);
+                    let mut route_two = (false, HashSet::new(), 0);
+
+                    // You open it, the elephant moves.
+                    for (one_name, one_node) in &self.nodes {
+                        let prev = table[min - 1].get(&(name1.to_string(), one_name.to_string())).unwrap();
+                        if prev.0 && !prev.1.contains(&node1.name) && one_node.tunnels.contains(name2) {
+                            let new_flow = prev.2 + node1.flow_rate * (time - min);
+                            if !route_one.0 || new_flow > route_one.2 {
+                                let mut set = prev.1.clone();
+                                set.insert(name1.clone());
+                                route_one = (true, set, new_flow);
+                            }
+                        }
+                    }
+
+                    // You move, the elephant opens it.
+                    for (two_name, two_node) in &self.nodes {
+                        let prev = table[min - 1].get(&(two_name.to_string(), name2.to_string())).unwrap();
+                        if prev.0 && !prev.1.contains(&node2.name) && two_node.tunnels.contains(name1) {
+                            let new_flow = prev.2 + node2.flow_rate * (time - min);
+                            if !route_two.0 || new_flow > route_two.2 {
+                                let mut set = prev.1.clone();
+                                set.insert(name2.clone());
+                                route_two = (true, set, new_flow);
+                            }
+                        }
+                    }
+
+                    // Or both of you move.
+                    let mut route_three = (false, HashSet::new(), 0);
+                    for (src1_name, src1_node) in &self.nodes {
+                        for (src2_name, src2_node) in &self.nodes {
+                            if src1_node.tunnels.contains(name1) && src2_node.tunnels.contains(name2) {
+                                let value = table[min - 1].get(&(src1_name.to_string(), src2_name.to_string())).unwrap();
+                                if value.0 && (!route_three.0 || value.2 > route_three.2) {
+                                    route_three = value.clone();
+                                }
+                            }
+                        }
+                    }
+
+                    println!("{}{}, {:?} {:?} {:?}", name1, name2, route_one, route_two, route_three);
+
+                    let best = [route_one, route_two, route_three].iter()
+                        .filter(|r| r.0)
+                        .max_by(|r1, r2| r1.2.cmp(&r2.2))
+                        .unwrap_or(&(false, HashSet::new(), 0)).clone();
+                    table[min].insert((name1.to_string(), name2.to_string()), best);
+                }
+            }
+        }
+
+        for row in table.iter().rev() {
+            for (k, v) in row {
+                print!("| {}{} {} {} ", k.0, k.1, v.0, v.2);
+            }
+            println!();
+        }
+
+        println!("{:?}", table[time].iter().max_by(|a, b| a.1.2.cmp(&b.1.2)).unwrap());
+        return table[time].iter().max_by(|a, b| a.1.2.cmp(&b.1.2)).unwrap().1.2;
     }
 }
--- day-16-dieciseis/src/main.rs
+++ day-16-dieciseis/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     let lines = Input::new().read_lines().lines();
     let graph = Graph::new(&lines);
 
-    println!("Answer for #1: {}", graph.solve_task(30));
+    //println!("Answer for #1: {}", graph.solve_task_v1(30));
+    println!("Answer for #2: {}", graph.solve_task_v2(26));
 }
